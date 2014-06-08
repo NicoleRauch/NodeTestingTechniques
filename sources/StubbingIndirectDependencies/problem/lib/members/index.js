c3239ffdbe0301dc5dda89accd99a51d9721945e
Nicole Rauch <nicole.m@gmx.de>
2014-06-08 16:21:12 +0200

    Still more renamings api -> service

--- sources/StubbingIndirectDependencies/problem/lib/members/index.js
+++ sources/StubbingIndirectDependencies/problem/lib/members/index.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var path = require('path');
-var api = require('./membersService');
+var service = require('./membersService');
 
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
@@ -10,7 +10,7 @@ app.set('view engine', 'jade');
 
 
 app.get('/', function (req, res, next) {
-  api.allMembers(function (err, members) {
+  service.allMembers(function (err, members) {
     if (err) { return next(err); }
     res.render('index', { members: members });
   });
--- sources/StubbingIndirectDependencies/solution/lib/members/index.js
+++ sources/StubbingIndirectDependencies/solution/lib/members/index.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var path = require('path');
 
-var api = require('nconf').get('beans').get('membersService');
+var service = require('nconf').get('beans').get('membersService');
 
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
@@ -11,7 +11,7 @@ app.set('view engine', 'jade');
 
 
 app.get('/', function (req, res, next) {
-  api.allMembers(function (err, members) {
+  service.allMembers(function (err, members) {
     if (err) { return next(err); }
     res.render('index', { members: members });
   });
