1879cec4cae815e5f0f3ab33aa3b293f24db57c6
Nicole Rauch <nicole.m@gmx.de>
2014-06-17 18:52:03 +0100

    Cleaned up the example test.

--- sources/DatabaseTests/problem/test/activities/activitiesService_test.js
+++ sources/DatabaseTests/problem/test/activities/activitiesService_test.js
@@ -2,14 +2,12 @@
 
 var sinon = require('sinon').sandbox.create();
 var expect = require('must');
-var _ = require('lodash');
 
 var beans = require('../../testutil/configureForTest').get('beans');
-
 var activitiesService = beans.get('activitiesService');
 var activitystore = beans.get('activitystore');
-
 var Activity = beans.get('activity');
+
 
 var dummyActivity = new Activity({title: 'Title of the Activity', description: 'description', assignedGroup: 'assignedGroup',
   location: 'location', direction: 'direction', startDate: '01.01.2013', url: 'urlOfTheActivity', color: 'aus Gruppe' });
@@ -27,8 +25,7 @@ describe('Activities Service', function () {
   it('returns all activities', function (done) {
     activitiesService.getActivitiesForDisplay(activitystore.allActivities, function (err, activities) {
       expect(activities.length).to.equal(1);
-      var activity = activities[0];
-      expect(activity.title()).to.equal('Title of the Activity');
+      expect(activities[0].title()).to.equal('Title of the Activity');
       done(err);
     });
   });
