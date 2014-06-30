19a6f7300cddd02579cb4acee5745c268fb11238
leider <derleider@web.de>
2014-06-30 09:20:49 +0100

    finishing slides (....)

--- sources/HTMLtests/solution/testutil/userStub.js
+++ sources/HTMLtests/solution/testutil/userStub.js
@@ -2,13 +2,11 @@
 
 module.exports = function (user) {
 
-  var userStub = function (req, res, next) {
+  return function (req, res, next) {
     req.user = user || {}; // for model checks
     res.locals.user = req.user; // for jade checks
     req.isAuthenticated = function () { return true; };
     next();
   };
-
-  return userStub;
 };
 
